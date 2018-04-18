fffede9939f4297eb32edbe8e335fbb70f82223a
guomin <yushi5344@gmail.com>
2018-04-18 21:06:15 +0800

    验证器与静态代理

---
+++ application/admin/controller/ValidateData.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use app\validate\User as Users;
+use app\facade\User;
+class Validatedata extends Controller{
+
+    public function index(){
+
+        $data=[
+            'name'=>'guomin',
+            'email'=>'424242qq.com',
+            'password'=>'222222',
+            'mobile'=>17092559941
+        ];
+
+        $validate=new Users();
+        if(!$validate->check($data)){
+            return $validate->getError();
+        }else{
+            return '通过';
+        }
+
+
+        /*
+        //静态代理
+       if(!User::check($data)) {
+           return User::getError();
+       }else{
+           return '通过';
+       }
+       */
+    }
+}
